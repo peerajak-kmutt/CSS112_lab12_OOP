606605b11d9ebb8e7d262f44833b1039099fe858
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-24 14:04:15 +0700

    Update Prob1_test.py

--- Prob1_test.py
+++ Prob1_test.py
@@ -2,8 +2,8 @@ import Prob1 as p1
 
 
 def test_1():
-    manans = "fight with Fist Hit Power is 3.0"
-    swordmanans = "fight with Sword Hit Power is 213"
+    manans = "fight with Fist Hit Power is 3.0 "
+    swordmanans = "fight with Sword Hit Power is 213 "
     p1ans = p1.Problem1()
     assert p1ans[0] == manans && p1ans[1] == swordmanans
 
