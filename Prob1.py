236c0faf7bb89473bc28f9f29c406a9c1bde3045
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-24 13:49:40 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,10 +1,43 @@
-def gen5odds():
-    #Your Code here
-    #End your code
+     
+class Item:
+    def __init__(self,hitPower):
+        self.hitPower =hitPower
+    
+class GamePeice:
+   
+    def fight(self):
+        return "fight with {} Hit Power is {} ".format(self.item.__class__.__name__,self.item.hitPower)
 
+class Sword(Item):
+    def __init__(self,hitPower,length):
+        super()#Your code here
+        #Your code here set object varibale length = length
+        
+class Fist(Item):
+    pass
+   
+    
+class Human(GamePeice):
+    
+    def __init__(self,height):
+        #Your code here set object varibale height = height
+        self.item = Fist(3.0)
+
+
+
+class Swordman(Human):
+    
+    def __init__(self,height,swardHitPower,length):        
+        super().#Your code here
+        self.item = Sword#Your code here
         
         
+
+
 def Problem1(): 
-    #Your code Here
-    #End your code
-    return []
+    aMan = Human(180)
+    msgMan = aMan.fight()
+
+    aSwordMan = Swordman(170,213,1.0)
+    msgSwordMan  = aSwordMan.fight()
+    return [msgMan, msgSwordMan]
