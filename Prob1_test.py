5ffd4147b661d78760e48de6caff7691a908c925
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-24 13:51:56 +0700

    Update Prob1_test.py

--- Prob1_test.py
+++ Prob1_test.py
@@ -2,14 +2,10 @@ import Prob1 as p1
 
 
 def test_1():
-    g9 =   p1.gen5odds() 
-    a1 = next(g9)
-    for k in range(8):
-        next(g9)
-    a10 = next(g9)
-    assert a1 == [1, 3, 5, 7, 9] and a10 == [91,93,95,97,99] 
+    manans = "fight with Fist Hit Power is 3.0"
+    swordmanans = "fight with Sword Hit Power is 213"
+    p1ans = Problem1()
+    assert p1ans[0] == manans && p1ans[1] == swordmanans
 
-def test_2():
-    a = p1.Problem1()
-    assert a == [25, 75, 125, 175, 225, 275, 325, 375, 425, 475]
+
 
